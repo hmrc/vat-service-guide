817376e73bb76858e529e46a425668a3b2b75f5b
Dave O'Brien <david.obrien@digital.hmrc.gov.uk>
2018-12-07 14:46:48 +0000

    Add sbt support

---
+++ project/AppDependencies.scala
@@ -0,0 +1,15 @@
+import play.core.PlayVersion
+import play.sbt.PlayImport._
+import sbt._
+
+object AppDependencies {
+  val bootStrapPlayVersion = "3.14.0"
+
+  val compile = Seq(
+    ws,
+    "uk.gov.hmrc" %% "bootstrap-play-25" % bootStrapPlayVersion)
+
+  val test = Seq(
+    "org.pegdown" % "pegdown" % "1.6.0" % "test",
+    "org.scalatest" %% "scalatest" % "3.0.5" % "test")
+}
