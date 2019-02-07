368c02d1665bf2897edd54a1623cc75da98175ee
Darren Walker <9861667+FluentSynergyDW@users.noreply.github.com>
2019-02-07 16:12:18 +0000

    APIS-4101 Add HotJar.

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,1 +1,10 @@
 //= require govuk_tech_docs
+
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:385813,hjsv:5};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
