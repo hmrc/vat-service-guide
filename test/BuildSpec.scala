2e925e768db799e813b84fddc1e5b1228afd98d1
Tapiwa Tiyemba <142234222+tapiwa-tiyemba@users.noreply.github.com>
2024-04-18 14:38:59 +0100

    fix typo

--- test/BuildSpec.scala
+++ test/BuildSpec.scala
@@ -24,7 +24,7 @@ class BuildSpec extends AnyWordSpec with Matchers {
     "produce static files" in {
       val result = "bundle install" #&& Process("bundle exec middleman build --build-dir=public/ --clean", None) !
 
-      result shouldBe 1
+      result shouldBe 0
     }
   }
 }
